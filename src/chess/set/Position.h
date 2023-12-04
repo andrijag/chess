83893bb37658c83dcbada111c022d337372e3ae4
Andrija Grozdanović <andrija.gro@gmail.com>
2023-12-04 18:05:37 +0100

    Add overloaded arithmetic operators

--- src/chess/set/Position.h
+++ src/chess/set/Position.h
@@ -1,9 +1,47 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-struct Position {
-    int row{};
-    int column{};
-};
+namespace chess
+{
+    struct Position
+    {
+        int row{};
+        int column{};
+    };
+
+    inline Position &operator+=(Position &a, const Position &b)
+    {
+        a.row += b.row;
+        a.column += b.column;
+        return a;
+    }
+
+    inline Position &operator-=(Position &a, const Position &b)
+    {
+        a.row -= b.row;
+        a.column -= b.column;
+        return a;
+    }
+
+    inline Position operator+(const Position &a)
+    {
+        return {a.row, a.column};
+    }
+
+    inline Position operator-(const Position &a)
+    {
+        return {-a.row, -a.column};
+    }
+
+    inline Position operator+(const Position &a, const Position &b)
+    {
+        return {a.row + b.row, a.column + b.column};
+    }
+
+    inline Position operator-(const Position &a, const Position &b)
+    {
+        return {a.row - b.row, a.column - b.column};
+    }
+}
 
 #endif
