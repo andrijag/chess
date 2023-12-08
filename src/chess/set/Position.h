096154ef5a164abb4c0697025c1cc5db51130952
Andrija Grozdanović <andrija.gro@gmail.com>
2023-12-08 17:51:46 +0100

    Add overloaded equality operators and hash function

--- src/chess/set/Position.h
+++ src/chess/set/Position.h
@@ -1,6 +1,9 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include <cstddef>
+#include <functional>
+
 namespace chess
 {
     struct Position
@@ -42,6 +45,25 @@ namespace chess
     {
         return {a.row - b.row, a.column - b.column};
     }
+
+    inline bool operator==(const Position &a, const Position &b)
+    {
+        return a.row == b.row && a.column == b.column;
+    }
+
+    inline bool operator!=(const Position &a, const Position &b)
+    {
+        return !(a == b);
+    }
 }
+
+template <>
+struct std::hash<chess::Position>
+{
+    std::size_t operator()(const chess::Position &p) const
+    {
+        return std::hash<int>()(p.row) ^ std::hash<int>()(p.column);
+    }
+};
 
 #endif
