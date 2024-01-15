119d24c6275927f11798719ed0777e7ae476cf2f
Andrija Grozdanović <andrija.gro@gmail.com>
2024-01-16 00:47:25 +0100

    Add Direction alias for Position and rm directions variable

--- src/chess/Direction.h
+++ src/chess/Direction.h
@@ -7,27 +7,18 @@
 
 namespace chess
 {
+    using Direction = Position;
+
     namespace direction
     {
-        constexpr Position n{-1, 0};
-        constexpr Position ne{-1, 1};
-        constexpr Position e{0, 1};
-        constexpr Position se{1, 1};
-        constexpr Position s{1, 0};
-        constexpr Position sw{1, -1};
-        constexpr Position w{0, -1};
-        constexpr Position nw{-1, -1};
-
-        constexpr int numberOfDirections{8};
-        constexpr std::array<Position, numberOfDirections> directions{
-            direction::n,
-            direction::ne,
-            direction::e,
-            direction::se,
-            direction::s,
-            direction::sw,
-            direction::w,
-            direction::nw};
+        constexpr Direction n{-1, 0};
+        constexpr Direction ne{-1, 1};
+        constexpr Direction e{0, 1};
+        constexpr Direction se{1, 1};
+        constexpr Direction s{1, 0};
+        constexpr Direction sw{1, -1};
+        constexpr Direction w{0, -1};
+        constexpr Direction nw{-1, -1};
     }
 }
 
